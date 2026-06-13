8bb3767e2cd50c2e0fab08f2e00a8df56a3ad8f3
Lakshay Tyagi <tyagilakshay119@gmail.com>
2026-06-13 15:37:19 +0530

    update biometric controller

--- app/controllers/api/biometric_mappings_controller.rb
+++ app/controllers/api/biometric_mappings_controller.rb
@@ -18,7 +18,8 @@ class Api::BiometricMappingsController < ApplicationController
     mapping = TrnMemberBiometricMapping.find_by(
       mbm_compcode:       params[:compcode],
       mbm_device_user_id: params[:device_user_id],
-      mbm_device_sn:      params[:device_sn]
+      mbm_device_sn:      params[:device_sn],
+      mbm_is_active:      'Y'
     )
     return render json: { status: false, message: "Mapping not found" } unless mapping
 
@@ -26,7 +27,6 @@ class Api::BiometricMappingsController < ApplicationController
       mbm_uid:             params[:uid],
       mbm_finger_template: params[:templates].to_json
     )
-
-    render json: { status: true }
+    render json: { status: true, mapping_id: mapping.id }
   end
 end
